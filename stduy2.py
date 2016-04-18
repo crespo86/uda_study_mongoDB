df462f9ab80fbbd49b5cc74fdc64ccaeb8b00e96
crespo86 <leesh1215@gmail.com>
2016-04-18 22:10:41 +0900

    study2 got a wrong answer.. and don't know why

--- stduy2.py
+++ stduy2.py
@@ -19,49 +19,62 @@ def open_zip(datafile):
 
 
 def parse_file(datafile):
-    workbook = xlrd.open_workbook(datafile)
-    sheet = workbook.sheet_by_index(0)
+    workbook = xlrd.open_workbook(datafile)  #load data
+    sheet = workbook.sheet_by_index(0)       #load data
 
-    ### example on how you can get the data
-    sheet_data = [[sheet.cell_value(r, col) for col in range(sheet.ncols)] for r in range(sheet.nrows)]
+    sheetdata = [[sheet.cell_value(r, col) for col in range(sheet.ncols)] for r in range(sheet.nrows)]
 
-    ### other useful methods:
-    print "\nROWS, COLUMNS, and CELLS:"
-    print "Number of rows in the sheet:",
-    print sheet.nrows
-    # print "Type of data in cell (row 3, col 2):",
-    # print sheet.cell_type(3, 2)
-    # print "Value in cell (row 3, col 2):",
-    # print sheet.cell_value(3, 2)
-    # print "Get a slice of values in column 3, from rows 1-3:"
-    # print sheet.col_values(3, start_rowx=1, end_rowx=4)
+    cv = sheet.col_values(1, start_rowx=1, end_rowx=None)
 
-    # print "\nDATES:"
-    # print "Type of data in cell (row 1, col 0):",
-    # print sheet.cell_type(1, 0)
-    # exceltime = sheet.cell_value(1, 0)
-    # print "Time in Excel format:",
-    # print exceltime
-    # print "Convert time to a Python datetime tuple, from the Excel float:",
-    # print xlrd.xldate_as_tuple(exceltime, 0)
+    maxval = max(cv)
+    minval = min(cv)
+
+    maxpos = cv.index(maxval) + 1
+    minpos = cv.index(minval) + 1
+
+    maxtime = sheet.cell_value(maxpos, 0)
+    realtime_max = xlrd.xldate_as_tuple(maxtime, 0)
+    mintime = sheet.cell_value(minpos, 0)
+    realtime_min = xlrd.xldate_as_tuple(mintime, 0)
+    avgcoast = sum(cv) / float(len(cv))
 
 
-#    data = {
-#            'maxtime': (0, 0, 0, 0, 0, 0),
-#            'maxvalue': 0,
-#            'mintime': (0, 0, 0, 0, 0, 0),
-#            'minvalue': 0,
-#            'avgcoast': 0
-#    }
-#    return data
 
+    data = {
+            'maxtime': realtime_max,
+            'maxvalue': maxtime,
+            'mintime': realtime_min,
+            'minvalue': mintime,
+            'avgcoast': avgcoast
+    }
+    print data
+    return data
 
 def test():
     open_zip(datafile)
     data = parse_file(datafile)
 
-#    assert data['maxtime'] == (2013, 8, 13, 17, 0, 0)
-#    assert round(data['maxvalue'], 10) == round(18779.02551, 10)
+    assert data['maxtime'] == (2013, 8, 13, 17, 0, 0)
+    assert round(data['maxvalue'], 10) == round(18779.02551, 10)
 
 
 test()
+
+    ### other useful methods:
+    #print "\nROWS, COLUMNS, and CELLS:"
+    #print "Number of rows in the sheet:",
+    #print sheet.nrows
+    #print "Type of data in cell (row 3, col 2):",
+    #print sheet.cell_type(3, 2)
+    #print "Value in cell (row 3, col 2):",
+    #print sheet.cell_value(3, 2)
+    #print "Get a slice of values in column 3, from rows 1-3:"
+    #print sheet.col_values(3, start_rowx=1, end_rowx=4)
+    #print "\nDATES:"
+    #print "Type of data in cell (row 1, col 0):",
+    #print sheet.cell_type(1, 0)
+    #exceltime = sheet.cell_value(1, 0)
+    #print "Time in Excel format:",
+    #print exceltime
+    #print "Convert time to a Python datetime tuple, from the Excel float:",
+    #print xlrd.xldate_as_tuple(exceltime, 0)
