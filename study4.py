429b96d6529506c248b1b3176bf9382bfaa068bf
crespo86 <leesh1215@gmail.com>
2016-04-20 00:27:48 +0900

    finish study4

---
+++ study4.py
@@ -0,0 +1,30 @@
+import xml.etree.ElementTree as ET
+import pprint
+
+tree = ET.parse('exampleResearchArticle.xml')
+root = tree.getroot()
+
+title = root.find('./fm/bibl/title')
+title_text = ""
+for p in title:
+    title_text += p.text
+print "\nTitle:\n", title_text
+
+print "\nAuthor email addresses:"
+for email in root.findall('./fm/bibl/aug/au/email'):
+    if email is not None:
+        print email.text
+
+
+
+
+
+
+
+
+
+
+
+
+#print "\nAuthor email addresses:"
+#for a in root.findall()
